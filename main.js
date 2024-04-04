b7e7b77159c408bc3dd8a0422f880bd02e58ff23
AnoushaBaig <164419082+AnoushaBaig@users.noreply.github.com>
2024-04-04 01:52:25 -0700

    data types covered

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log("Hello world!")
+// use of let
+// let name1 = "hello anosha"; 
+// console.log(name1)
+// use of var
+// var name1 ="variable by var";
+// var name2 ="anousha";
+// console.log(name1);
+// let name1 ="anousha"
+// console.log(name1)
+// name1 = "ubaid"
+// console.log(name1)
+// use of const
+// const name1 ="Anousha"
+// name1 ="ubaid"
+// console.log(name1) 
+// data types
+var name1 = "anousha"; // inferring type //wo khud guess krega 
+var name2 = "anousha"; //stroung data type /// hm btayengy
+// name2 =10000
+var num1 = 1000;
+var num2 = 2000;
+var news = false; // inferring typed
+var news1 = true; // strongly typed
+var anyData = 1000; // not recomanded
+anyData = "hello"; // update krna
+anyData = true;
---
+++ main.ts
@@ -0,0 +1,35 @@
+// console.log("Hello world!")
+// use of let
+// let name1 = "hello anosha"; 
+// console.log(name1)
+
+// use of var
+// var name1 ="variable by var";
+// var name2 ="anousha";
+// console.log(name1);
+
+// let name1 ="anousha"
+// console.log(name1)
+// name1 = "ubaid"
+// console.log(name1)
+
+// use of const
+// const name1 ="Anousha"
+// name1 ="ubaid"
+// console.log(name1) 
+
+// data types
+let name1 ="anousha"// inferring type //wo khud guess krega 
+let name2:string ="anousha" //stroung data type /// hm btayengy
+// name2 =10000
+let num1 =1000
+let num2 :number =2000
+
+let news = false // inferring typed
+let news1 :boolean = true// strongly typed
+
+let anyData : any =1000// not recomanded
+anyData ="hello" // update krna
+anyData = true
+
+
